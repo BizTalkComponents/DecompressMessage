95313b5ca61d9c98c8d93b54e1ab1e9ca4f50856
Robin Hultman <robin.hultman@gmail.com>
2016-01-27 18:56:45 +0100

    Added unit test on file names

--- Tests/UnitTests/ZipDisassemblerTests.cs
+++ Tests/UnitTests/ZipDisassemblerTests.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Winterdom.BizTalk.PipelineTesting;
 using System.IO;
+using BizTalkComponents.Utils;
 
 namespace BizTalkComponents.PipelineComponents.DecompressMessage.Tests.UnitTests
 {
@@ -27,6 +28,28 @@ namespace BizTalkComponents.PipelineComponents.DecompressMessage.Tests.UnitTests
                     Assert.AreEqual(expectedNoOfFilesInZip, output.Count);
                 }
             }
+        }
+
+        [TestMethod]
+        public void TestCorrectFileNames()
+        {
+            var pipeline = PipelineFactory.CreateEmptyReceivePipeline();
+            var component = new ZipDisassembler();
+            string zipPath = @"TestData\test.zip";
+            pipeline.AddComponent(component, PipelineStage.Disassemble);
+            MessageCollection output;
+            using (FileStream fs = new FileStream(zipPath, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    var message = MessageHelper.CreateFromStream(sr.BaseStream);
+                    output = pipeline.Execute(message);
+                }
+            }
+
+            Assert.AreEqual("testfile1.txt", output[0].Context.Read(new ContextProperty(FileProperties.ReceivedFileName)));
+            Assert.AreEqual("testfile2.txt", output[1].Context.Read(new ContextProperty(FileProperties.ReceivedFileName)));
+            Assert.AreEqual("testfile3.txt", output[2].Context.Read(new ContextProperty(FileProperties.ReceivedFileName)));
         }
     }
 }
